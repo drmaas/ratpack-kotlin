2336fb34eca8d7e9e811d6d1b9f490e8f27fdb8e
Adan Perez <nadazerep@gmail.com>
2018-06-12 08:44:03 -0500

    feat: Add KServerSpec.serverConfig DSL to accept Actions.

--- ratpack-kotlin-dsl/src/main/kotlin/ratpack/kotlin/handling/KServerSpec.kt
+++ ratpack-kotlin-dsl/src/main/kotlin/ratpack/kotlin/handling/KServerSpec.kt
@@ -1,11 +1,13 @@
 package ratpack.kotlin.handling
 
+import ratpack.func.Action
 import ratpack.guice.BindingsSpec
 import ratpack.guice.Guice
 import ratpack.server.RatpackServerSpec
 import ratpack.server.ServerConfigBuilder
 
 class KServerSpec(val delegate: RatpackServerSpec) {
+    fun serverConfig(action: Action<in ServerConfigBuilder>) : KServerSpec = KServerSpec(delegate.serverConfig(action))
     inline fun serverConfig(crossinline cb: ServerConfigBuilder.(s: ServerConfigBuilder) -> Unit) : KServerSpec = KServerSpec(delegate.serverConfig { it.cb(it) })
     inline fun bindings(crossinline cb: BindingsSpec.(b: BindingsSpec) -> Unit) : KServerSpec = KServerSpec(delegate.registry(Guice.registry { it.cb(it) }))
     inline fun handlers(crossinline cb: KChain.(c: KChain) -> Unit) : KServerSpec = KServerSpec(delegate.handlers { val c = KChain(it); c.cb(c) })
