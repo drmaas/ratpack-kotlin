8b4807fff1a3a2bb798f56bf7fd51176f31bbd4b
Adan Perez <nadazerep@gmail.com>
2018-06-14 10:46:18 -0500

    feat: Add KServerSpec.serverConfig DSL to accept Actions. (#19)

---
+++ ratpack-kotlin-dsl/src/test/kotlin/ratpack/kotlin/handling/KServerSpecTest.kt
@@ -0,0 +1,27 @@
+package ratpack.kotlin.handling
+
+import io.kotlintest.matchers.shouldEqual
+import io.kotlintest.specs.BehaviorSpec
+import ratpack.func.Action
+import ratpack.kotlin.test.embed.ratpack
+
+class KServerSpecTest : BehaviorSpec() {
+  init {
+    // ratpack serverConfig(action: Action<in ServerConfigBuilder>) dsl test
+    given("a ratpack server") {
+      val app = ratpack {
+        serverConfig(Action() {
+          it.port(1234)
+        })
+        bindings {}
+        handlers {}
+      }
+      `when`("the application runs") {
+        then("serverConfig(action: Action<in ServerConfigBuilder>) works") {
+          app.address.toString() shouldEqual "http://localhost:1234/"
+          app.close()
+        }
+      }
+    }
+  }
+}
