9774e76b14de2073aba39b62d05e0333eef95a25
DanMaas <daniel.maas@target.com>
2020-01-13 14:31:33 -0600

    make kotlin extensions to http client more modular

--- ratpack-kotlin-dsl/src/main/kotlin/ratpack/kotlin/client/KHttpClient.kt
+++ ratpack-kotlin-dsl/src/main/kotlin/ratpack/kotlin/client/KHttpClient.kt
@@ -7,88 +7,108 @@ import ratpack.http.client.ReceivedResponse
 import ratpack.http.client.RequestSpec
 import java.net.URI
 
-fun HttpClient.get(uri: String, action: (RequestSpec) -> Unit): Promise<ReceivedResponse> {
-  return get(URI.create(uri), action)
-}
+class KHttpClient(val client: HttpClient) : HttpClient by client {
+  fun get(uri: String, action: (RequestSpec) -> Unit): Promise<ReceivedResponse> {
+    return get(URI.create(uri), action)
+  }
 
-fun HttpClient.get(uri: String): Promise<ReceivedResponse> {
-  val action: (RequestSpec) -> Unit = {}
-  return get(uri, action)
-}
+  fun get(uri: String): Promise<ReceivedResponse> {
+    val action: (RequestSpec) -> Unit = {}
+    return get(uri, action)
+  }
 
-fun HttpClient.put(uri: URI, action: Action<in RequestSpec>): Promise<ReceivedResponse> {
-  return request(uri, action.prepend { it.put() })
-}
+  fun put(uri: URI, action: Action<in RequestSpec>): Promise<ReceivedResponse> {
+    return request(uri, action.prepend { it.put() })
+  }
 
-fun HttpClient.put(uri: URI): Promise<ReceivedResponse> {
-  return put(uri, Action.noop())
-}
+  fun put(uri: URI): Promise<ReceivedResponse> {
+    return put(uri, Action.noop())
+  }
 
-fun HttpClient.put(uri: String, action: (RequestSpec) -> Unit): Promise<ReceivedResponse> {
-  return put(URI.create(uri), Action { action(it) })
-}
+  fun put(uri: String, action: (RequestSpec) -> Unit): Promise<ReceivedResponse> {
+    return put(URI.create(uri), Action { action(it) })
+  }
 
-fun HttpClient.put(uri: String): Promise<ReceivedResponse> {
-  val action: (RequestSpec) -> Unit = {}
-  return put(uri, action)
-}
+  fun put(uri: String): Promise<ReceivedResponse> {
+    val action: (RequestSpec) -> Unit = {}
+    return put(uri, action)
+  }
 
-fun HttpClient.post(uri: String, action: (RequestSpec) -> Unit): Promise<ReceivedResponse> {
-  return post(URI.create(uri), action)
-}
+  fun post(uri: String, action: (RequestSpec) -> Unit): Promise<ReceivedResponse> {
+    return post(URI.create(uri), action)
+  }
 
-fun HttpClient.post(uri: String): Promise<ReceivedResponse> {
-  val action: (RequestSpec) -> Unit = {}
-  return post(uri, action)
-}
+  fun post(uri: String): Promise<ReceivedResponse> {
+    val action: (RequestSpec) -> Unit = {}
+    return post(uri, action)
+  }
 
-fun HttpClient.delete(uri: URI, action: Action<in RequestSpec>): Promise<ReceivedResponse> {
-  return request(uri, action.prepend { it.delete() })
-}
+  fun delete(uri: URI, action: Action<in RequestSpec>): Promise<ReceivedResponse> {
+    return client.request(uri, action.prepend { it.delete() })
+  }
 
-fun HttpClient.delete(uri: URI): Promise<ReceivedResponse> {
-  return delete(uri, Action.noop())
-}
+  fun delete(uri: URI): Promise<ReceivedResponse> {
+    return delete(uri, Action.noop())
+  }
 
-fun HttpClient.delete(uri: String, action: (RequestSpec) -> Unit): Promise<ReceivedResponse> {
-  return delete(URI.create(uri), Action { action(it) })
-}
+  fun delete(uri: String, action: (RequestSpec) -> Unit): Promise<ReceivedResponse> {
+    return delete(URI.create(uri), Action { action(it) })
+  }
 
-fun HttpClient.delete(uri: String): Promise<ReceivedResponse> {
-  val action: (RequestSpec) -> Unit = {}
-  return delete(uri, action)
-}
+  fun delete(uri: String): Promise<ReceivedResponse> {
+    val action: (RequestSpec) -> Unit = {}
+    return delete(uri, action)
+  }
 
-fun HttpClient.patch(uri: URI, action: Action<in RequestSpec>): Promise<ReceivedResponse> {
-  return request(uri, action.prepend { it.patch() })
-}
+  fun patch(uri: URI, action: Action<in RequestSpec>): Promise<ReceivedResponse> {
+    return request(uri, action.prepend { it.patch() })
+  }
 
-fun HttpClient.patch(uri: URI): Promise<ReceivedResponse> {
-  return patch(uri, Action.noop())
-}
+  fun patch(uri: URI): Promise<ReceivedResponse> {
+    return patch(uri, Action.noop())
+  }
 
-fun HttpClient.patch(uri: String, action: (RequestSpec) -> Unit): Promise<ReceivedResponse> {
-  return patch(URI.create(uri), Action { action(it) })
-}
+  fun patch(uri: String, action: (RequestSpec) -> Unit): Promise<ReceivedResponse> {
+    return patch(URI.create(uri), Action { action(it) })
+  }
 
-fun HttpClient.patch(uri: String): Promise<ReceivedResponse> {
-  val action: (RequestSpec) -> Unit = {}
-  return patch(uri, action)
-}
+  fun patch(uri: String): Promise<ReceivedResponse> {
+    val action: (RequestSpec) -> Unit = {}
+    return patch(uri, action)
+  }
 
-fun HttpClient.options(uri: URI, action: Action<in RequestSpec>): Promise<ReceivedResponse> {
-  return request(uri, action.prepend { it.options() })
-}
+  fun options(uri: URI, action: Action<in RequestSpec>): Promise<ReceivedResponse> {
+    return request(uri, action.prepend { it.options() })
+  }
 
-fun HttpClient.options(uri: URI): Promise<ReceivedResponse> {
-  return options(uri, Action.noop())
-}
+  fun options(uri: URI): Promise<ReceivedResponse> {
+    return options(uri, Action.noop())
+  }
 
-fun HttpClient.options(uri: String, action: (RequestSpec) -> Unit): Promise<ReceivedResponse> {
-  return options(URI.create(uri), Action { action(it) })
-}
+  fun options(uri: String, action: (RequestSpec) -> Unit): Promise<ReceivedResponse> {
+    return options(URI.create(uri), Action { action(it) })
+  }
 
-fun HttpClient.options(uri: String): Promise<ReceivedResponse> {
-  val action: (RequestSpec) -> Unit = {}
-  return options(uri, action)
+  fun options(uri: String): Promise<ReceivedResponse> {
+    val action: (RequestSpec) -> Unit = {}
+    return options(uri, action)
+  }
+
+  fun head(uri: URI, action: Action<in RequestSpec>): Promise<ReceivedResponse> {
+    return request(uri, action.prepend { it.head() })
+  }
+
+  fun head(uri: URI): Promise<ReceivedResponse> {
+    return head(uri, Action.noop())
+  }
+
+  fun head(uri: String, action: (RequestSpec) -> Unit): Promise<ReceivedResponse> {
+    return head(URI.create(uri), Action { action(it) })
+  }
+
+  fun head(uri: String): Promise<ReceivedResponse> {
+    val action: (RequestSpec) -> Unit = {}
+    return head(uri, action)
+  }
+
 }
